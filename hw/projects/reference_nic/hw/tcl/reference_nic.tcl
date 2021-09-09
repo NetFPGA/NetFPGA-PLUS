c26d32c19a23e6adf7cb391bab7d79016b6666ff
Yuta <yuta.netfpga@gmail.com>
2021-09-09 23:26:49 +0900

    add cdc ip

--- hw/projects/reference_nic/hw/tcl/reference_nic.tcl
+++ hw/projects/reference_nic/hw/tcl/reference_nic.tcl
@@ -222,6 +222,25 @@ set_property generate_synth_checkpoint false [get_files axi_crossbar_0.xci]
 reset_target all [get_ips axi_crossbar_0]
 generate_target all [get_ips axi_crossbar_0]
 
+create_ip -name axi_clock_converter -vendor xilinx.com -library ip -module_name axi_clock_converter_0
+set_property -dict {
+    CONFIG.PROTOCOL {AXI4LITE}
+    CONFIG.DATA_WIDTH {32}
+    CONFIG.ID_WIDTH {0}
+    CONFIG.AWUSER_WIDTH {0}
+    CONFIG.ARUSER_WIDTH {0}
+    CONFIG.RUSER_WIDTH {0}
+    CONFIG.WUSER_WIDTH {0}
+    CONFIG.BUSER_WIDTH {0}
+    CONFIG.SI_CLK.FREQ_HZ {250000000}
+    CONFIG.MI_CLK.FREQ_HZ {340000000}
+    CONFIG.ACLK_ASYNC {1}
+    CONFIG.SYNCHRONIZATION_STAGES {3}
+} [get_ips axi_clock_converter_0]
+set_property generate_synth_checkpoint false [get_files axi_clock_converter_0.xci]
+reset_target all [get_ips axi_clock_converter_0]
+generate_target all [get_ips axi_clock_converter_0]
+
 #Add a clock wizard
 create_ip -name clk_wiz -vendor xilinx.com -library ip -module_name clk_wiz_1
 if {[string match "${datapath_freq_mhz}" "200"]} {

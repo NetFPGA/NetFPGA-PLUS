caa6ba713566252797fa5911232915524b7ea172
Yuta <yuta.netfpga@gmail.com>
2021-09-11 00:10:44 +0900

    update au200 timing

--- hw/projects/reference_router/hw/constraints/au200_vcu1525_user_timing.tcl
+++ hw/projects/reference_router/hw/constraints/au200_vcu1525_user_timing.tcl
@@ -25,5 +25,6 @@
 
 create_pblock pblock_nf_datapath
 add_cells_to_pblock [get_pblocks pblock_nf_datapath] [get_cells -quiet [list nf_datapath_0]]
+add_cells_to_pblock [get_pblocks pblock_nf_datapath] [get_cells -quiet [list u_top_wrapper/u_nf_attachment]]
 resize_pblock [get_pblocks pblock_nf_datapath] -add {SLR2}
 
